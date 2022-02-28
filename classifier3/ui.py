94e6d6d57051a69d61ad1ce7d6998d622582fd8c
Alexis-Matheson <82848478+Alexis-Matheson@users.noreply.github.com>
2022-02-28 10:40:51 -0800

    Changed layout of the ui
    Added popup menu for settings. Replaced the old grid system layout with a new pack/place. Added a main menu for navigation, and returning to the main menu will terminate any current processes.

--- classifier3/ui.py
+++ classifier3/ui.py
@@ -13,54 +13,81 @@ root.iconbitmap('shark.ico') #Change icon at top left corner use same root
 root.geometry("750x350")
 
 # Globals
-boolean = False
+is_file = False             # Boolean used to check for valid file
+
 enable_images = IntVar()    # Tkinter integer variable
 
 def open_file(root):
+    global stop_program
+    stop_program = False        # Used to terminate processes if the user wants to "go back" while the program is running
+
     home = "C:/"        # Default directory for file explorer
     video_file = Path(filedialog.askopenfilename(title = "select", initialdir = home))
-    boolean = True
-    if (boolean == True):
+    is_file = True
+    if (is_file):
         # Hide open file button and label while the program processes a selected file
-        #btn_label.grid_forget()
-        #open_file_btn.grid_forget()
+        open_file_btn.place_forget()
+        open_folder_btn.place_forget()
 
         # Set up progress bar
-        progress_label = Label(root, text="Loading Video File...")
-        progress_label.grid(column = 3, row = 0)
-        progress_bar = ttk.Progressbar(root, orient=HORIZONTAL, length=200)
-        progress_bar.grid(column=4, row=0)
+        progress_label.place(relx=0.28, rely=0.5, anchor=CENTER)
+        progress_bar.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+        # Go Back Button (Stops Process)
+        back_btn.place(relx=0, rely=0, anchor=NW)
 
         # Start the video processing
         begin(root, progress_bar, video_file)
 
         # completion message
-        test2 = Label(root, text = f"Finished labeling frames, output is located in: output.csv")
-        test2.grid(column=0, row=2)
-
-        btn_label.grid(column=0, row=0)
-        open_file_btn.grid(column=1, row=0)
+        done_lbl = Label(root, text = f"Finished labeling frames, output is located in: output.csv")
+        done_lbl.place(relx=0, rely=0, anchor=SW)
 
 
-btn_label = Label(root, text="Click to Select Video File: ")
-btn_label.grid(column=0, row=0)
+def open_settings():
+    window = Toplevel(root)
 
-open_file_btn = Button(root, text = "Open File", command =lambda: open_file(root))
-open_file_btn.grid(column=1, row=0)
+    # # Enable image output setting
+    check_box = Checkbutton(window, text='Output Images',variable=enable_images, onvalue=1, offvalue=0)
+    check_box.pack()
 
-# Application settings label
-settings_label = Label(root, text="Settings")
-settings_label.grid(column = 0, row = 6)
+    # # Confidence Level setting
+    slider = Scale(window, from_=0, to=100, orient=HORIZONTAL)
+    slider.pack()
 
-# Enable image output setting
-check_box = Checkbutton(root, text='Output Images',variable=enable_images, onvalue=1, offvalue=0)
-check_box.grid(column = 0, row = 7)
+def reset_screen():
+    classifier.stop_prog = True
+    back_btn.place_forget()
+    progress_label.place_forget()
+    progress_bar.place_forget()
 
-# Confidence Level setting
-slider = Scale(root, from_=0, to=100, orient=HORIZONTAL)
-slider.grid(column = 0, row = 8)
+    open_file_btn.place(relx=0.5, rely=0.4, anchor=CENTER)
+    open_folder_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+    quit_btn.place(relx=0.5, rely=0.6, anchor=CENTER)
 
 def begin(root, progress_bar, video_file):
-    classifier.main(root, progress_bar, video_file, slider)
+    classifier.main(root, progress_bar, video_file)
+
+# Initializers
+back_btn = Button(root, text = "<--- Return to Menu", command = reset_screen)
+progress_label = Label(root, text="Loading Video File...")
+progress_bar = ttk.Progressbar(root, orient=HORIZONTAL, length=200)
+
+# Open a single file
+open_file_btn = Button(root, text = "Open Single File", command =lambda: open_file(root))
+open_file_btn.place(relx=0.5, rely=0.4, anchor=CENTER)
+
+# Open a folder of videos
+open_folder_btn = Button(root, text = "Open Folder", command =lambda: open_file(root))
+open_folder_btn.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+# Quit the program
+quit_btn = Button(root, text = "Quit Program", command = root.destroy)
+quit_btn.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+# Application settings
+img = PhotoImage(file = 'SettingsIcon2.png')
+setting_btn = Button(root, image = img, command = open_settings)
+setting_btn.pack(side=TOP, anchor=NE)
 
 root.mainloop()
